41d6ff20bded2fe9d63c5e49c76821f710d55297
Mas hary <mas32957@gmail.com>
2020-05-11 19:23:31 +0700

    function paging

--- app/Http/Responses/DataMaster/Kategori/IndexResponse.php
+++ app/Http/Responses/DataMaster/Kategori/IndexResponse.php
@@ -6,9 +6,14 @@ use Illuminate\Contracts\Support\Responsable;
 class IndexResponse implements Responsable{
 	protected $limit = 10;
 	public function toResponse($request){
-		$kategoris          = Kategori::sort()->paginate($this->limit);
+		$kategoris          = Kategori::sort()->limit();
         return view('dashboard.kategori.index',[
             'kategoris'     => $kategoris
         ]);
+	}
+
+	public function limit($query){
+		$limit = 10;
+		return $query->paginate($limit);
 	}
 }
